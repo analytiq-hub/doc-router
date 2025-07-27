ec2547cfd9a60673cc26be360a0933c6b6b261bb
Andrei Radulescu-Banu <44415560+andrei-radulescu-banu@users.noreply.github.com>
2025-07-26 22:09:32 -0400

    devel

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,13 +1,5 @@
 import "@/styles/globals.css";
 import React from 'react';
-import SessionProvider from "@/components/SessionProvider"
-import Layout from '@/components/Layout';
-import ThemeRegistry from '@/components/ThemeRegistry';
-import { OrganizationProvider } from '@/contexts/OrganizationContext';
-import { getAppServerSession } from '@/utils/session';
-import { Toaster } from 'react-hot-toast';
-import { ToastContainer } from 'react-toastify';
-import FormioProvider from '@/components/FormioProvider';
 
 export const metadata = {
   title: 'Smart Document Router',
@@ -17,26 +9,15 @@ export const metadata = {
   },
 };
 
-export default async function RootLayout({
+export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  const appSession = await getAppServerSession();
   return (
     <html lang="en">
       <body>
-        <ThemeRegistry>
-          <SessionProvider session={appSession}>
-            <OrganizationProvider>
-              <FormioProvider>
-                <Layout>{children}</Layout>
-              </FormioProvider>
-            </OrganizationProvider>
-          </SessionProvider>
-        </ThemeRegistry>
-        <ToastContainer position="top-right" />
-        <Toaster position="top-right" />
+        {children}
       </body>
     </html>
   );
