c456dc2fcf197127cb8cb755002f4aa118689ebd
Andrei Radulescu-Banu <44415560+andrei-radulescu-banu@users.noreply.github.com>
2026-04-22 13:04:23 -0400

    devel

--- packages/python/app/flows/nodes/__init__.py
+++ packages/python/app/flows/nodes/__init__.py
@@ -1,5 +1,5 @@
-from .manual_trigger_node import DocRouterManualTriggerNode
-from .ocr_node import DocRouterOcrNode
-from .llm_node import DocRouterLlmExtractNode
-from .tag_node import DocRouterSetTagsNode
+from .manual_trigger_node import *
+from .ocr_node import *
+from .llm_node import *
+from .tag_node import *
 
