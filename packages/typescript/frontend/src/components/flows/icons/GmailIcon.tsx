7215197abab6974ceffb5df4c42a261b2fd7f3d4
Andrei Radulescu-Banu <44415560+andrei-radulescu-banu@users.noreply.github.com>
2026-06-25 08:08:56 -0400

    devel

--- packages/typescript/frontend/src/components/flows/icons/GmailIcon.tsx
+++ packages/typescript/frontend/src/components/flows/icons/GmailIcon.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
 
-/** Gmail product mark (paths from n8n ``gmail.svg``). */
+/** Gmail product mark — paths from Wikimedia Commons (Gmail icon 2020). */
 export function GmailIcon({ className }: { className?: string }): React.ReactElement {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 256 193"
+      viewBox="52 42 88 66"
       preserveAspectRatio="xMidYMid"
       className={className}
       aria-hidden
     >
-      <path fill="#4285F4" d="M58.182 192.05V93.14L27.507 65.077 0 49.504v125.091c0 9.658 7.825 17.455 17.455 17.455z" />
-      <path fill="#34A853" d="M197.818 192.05h40.727c9.659 0 17.455-7.826 17.455-17.455V49.505l-31.156 17.837-27.026 25.798z" />
-      <path fill="#EA4335" d="m58.182 93.14-4.174-38.647 4.174-36.989L128 69.868l69.818-52.364 4.67 34.992-4.67 40.644L128 145.504z" />
-      <path fill="#FBBC04" d="M197.818 17.504V93.14L256 49.504V26.231c0-21.585-24.64-33.89-41.89-20.945z" />
-      <path fill="#C5221F" d="m0 49.504 26.759 20.07L58.182 93.14V17.504L41.89 5.286C24.61-7.66 0 4.646 0 26.23z" />
+      <path fill="#4285f4" d="M58 108h14V74L52 59v43c0 3.32 2.69 6 6 6" />
+      <path fill="#34a853" d="M120 108h14c3.32 0 6-2.69 6-6V59l-20 15" />
+      <path fill="#fbbc04" d="M120 48v26l20-15v-8c0-7.42-8.47-11.65-14.4-7.2" />
+      <path fill="#ea4335" d="M72 74V48l24 18 24-18v26L96 92" />
+      <path fill="#c5221f" d="M52 51v8l20 15V48l-5.6-4.2c-5.94-4.45-14.4-.22-14.4 7.2" />
     </svg>
   );
 }
--- packages/typescript/frontend/src/components/flows/icons/GoogleDriveIcon.tsx
+++ packages/typescript/frontend/src/components/flows/icons/GoogleDriveIcon.tsx
@@ -1,22 +1,33 @@
 import React from 'react';
 
-/** Google Drive product mark (paths from n8n `googleDrive.svg`). */
+/** Google Drive product mark — paths from Wikimedia Commons (Google Drive icon 2020). */
 export function GoogleDriveIcon({ className }: { className?: string }): React.ReactElement {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 81 73"
-      className={className}
-      aria-hidden
-    >
-      <g fillRule="nonzero">
-        <path fill="#0066da" d="m6.548 61.76 3.528 6.094c.733 1.283 1.787 2.291 3.024 3.024l12.6-21.81H.5a8.3 8.3 0 0 0 1.1 4.124z" />
-        <path fill="#00ac47" d="M40.5 22.91 27.9 1.1c-1.237.733-2.291 1.741-3.024 3.024L1.6 44.445a8.3 8.3 0 0 0-.5 4.123h25.2z" />
-        <path fill="#ea4335" d="m67.9 70.878c1.237-.733 2.291-1.741 3.024-3.024l1.466-2.52 7.01-12.142a8.3 8.3 0 0 0 1.1-4.124H55.298l5.363 10.538z" />
-        <path fill="#00832d" d="M40.5 22.91 53.1 1.1C51.863.367 50.443 0 48.977 0H32.024c-1.466 0-2.887.412-4.124 1.1z" />
-        <path fill="#2684fc" d="M55.299 48.568H25.7l-12.6 21.81c1.237.733 2.657 1.1 4.124 1.1h46.552c1.466 0 2.887-.412 4.124-1.1z" />
-        <path fill="#ffba00" d="M67.762 24.784 56.124 4.624C55.391 3.341 54.337 2.333 53.1 1.6L40.5 23.41l14.8 25.659h25.155a8.3 8.3 0 0 0-1.1-4.124z" />
-      </g>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 87.3 78" className={className} aria-hidden>
+      <path
+        d="m6.6 66.85 3.85 6.65c.8 1.4 1.95 2.5 3.3 3.3l13.75-23.8h-27.5c0 1.55.4 3.1 1.2 4.5z"
+        fill="#0066da"
+      />
+      <path
+        d="m43.65 25-13.75-23.8c-1.35.8-2.5 1.9-3.3 3.3l-25.4 44a9.06 9.06 0 0 0-1.2 4.5h27.5z"
+        fill="#00ac47"
+      />
+      <path
+        d="m73.55 76.8c1.35-.8 2.5-1.9 3.3-3.3l1.6-2.75 7.65-13.25c.8-1.4 1.2-2.95 1.2-4.5h-27.502l5.852 11.5z"
+        fill="#ea4335"
+      />
+      <path
+        d="m43.65 25 13.75-23.8c-1.35-.8-2.9-1.2-4.5-1.2h-18.5c-1.6 0-3.15.45-4.5 1.2z"
+        fill="#00832d"
+      />
+      <path
+        d="m59.8 53h-32.3l-13.75 23.8c1.35.8 2.9 1.2 4.5 1.2h50.8c1.6 0 3.15-.45 4.5-1.2z"
+        fill="#2684fc"
+      />
+      <path
+        d="m73.4 26.5-12.7-22c-.8-1.4-1.95-2.5-3.3-3.3l-13.75 23.8 16.15 28h27.45c0-1.55-.4-3.1-1.2-4.5z"
+        fill="#ffba00"
+      />
     </svg>
   );
 }
